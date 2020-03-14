e47b5080b308dd3cda55a0b4e2ddb9204ca45fc8
stNamco <lbnamco15@gmail.com>
2020-03-14 14:42:39 +0900

    replace viewcontroller to tableviewcontroller to avoid redundant implementation

--- Example/Example/MainViewController.swift
+++ Example/Example/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
